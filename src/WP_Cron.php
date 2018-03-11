3a352a87b6a5422404332ce72e18afa0053a42e5
Anthony Budd <anthonybudd94@gmail.com>
2018-03-11 19:47:21 +0000

    v1.1

--- src/WP_Cron.php
+++ src/WP_Cron.php
@@ -13,33 +13,32 @@ Class WP_Cron{
 	}
 
 	public function calculateInterval(){
-		if(is_array($this->interval)){
-			$interval = 0;
 
-			$multipliers = array(
-				'seconds' 	=> 1,
-				'minutes' 	=> 60,
-				'hours' 	=> 3600,
-				'days' 		=> 86400,
-				'weeks' 	=> 604800,
-				'months' 	=> 2628000,
-			);
-
-			foreach($multipliers as $unit => $multiplier){
-				if(isset($this->interval[$key]) && is_int($this->interval[$key])){
-					$interval = $interval + ($this->interval[$key] * $multiplier);
-				}
-			}
-
-			return $interval;
-
-		}else if(is_int($this->interval)){
-			return $this->interval;
-		}else{
-			// parse string??
+		if(!is_array($this->interval)){
+			throw new Exception("Interval must be an array");
 		}
 
-		throw new Exception("Interval not valid");
+		if(!(count(array_filter(array_keys($array), 'is_string')) > 0)){
+			throw new Exception("WP_Cron::\$interval must be an assoc array");
+		}
+		
+		$interval = 0;
+		$multipliers = array(
+			'seconds' 	=> 1,
+			'minutes' 	=> 60,
+			'hours' 	=> 3600,
+			'days' 		=> 86400,
+			'weeks' 	=> 604800,
+			'months' 	=> 2628000,
+		);
+
+		foreach($multipliers as $unit => $multiplier){
+			if(isset($this->interval[$key]) && is_int($this->interval[$key])){
+				$interval = $interval + ($this->interval[$key] * $multiplier);
+			}
+		}
+
+		return $interval;
 	}
 
 	public function scheduleFilter($schedules){
